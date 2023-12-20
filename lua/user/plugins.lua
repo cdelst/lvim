da8842e7bac8e41b74ff15486956f3d3ae18d4af
cdelst <casedelst@gmail.com>
2023-12-20 00:16:44 -0800

    add dropbar but then took it out because it needs 0.10

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -190,7 +190,17 @@ lvim.plugins = {
       require("gitblame").setup { enabled = false }
     end,
   },
-
+  -- Dropbar requires 0.10.0
+  -- {
+  --   "Bekaboo/dropbar.nvim",
+  --   -- optional, but required for fuzzy finder support
+  --   dependencies = {
+  --     "nvim-telescope/telescope-fzf-native.nvim",
+  --   },
+  --   config = function()
+  --     require("dropbar").setup()
+  --   end,
+  -- },
   -- "folke/noice.nvim",
   -- "rcarriga/nvim-notify",
 
