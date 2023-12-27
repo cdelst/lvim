383c91eff29ccb7ddd5b7f2b798cd1fffa66c508
Case Delst <casedelst@gmail.com>
2023-12-26 17:14:33 -0800

    install leetcode.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -218,4 +218,19 @@ lvim.plugins = {
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-treesitter/nvim-treesitter", "rcarriga/nvim-notify", "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      -- configuration goes here
+    },
+  },
 }
