06d150a749bf5ea91ca1e7eea65bdbfd3ba6e459
Case Delst <casedelst@gmail.com>
2023-12-26 17:14:27 -0800

    Fix bug with modicator

--- lua/user/modicator.lua
+++ lua/user/modicator.lua
@@ -4,8 +4,17 @@ require("modicator").setup {
   highlights = {
     -- Default options for bold/italic
     defaults = {
-      bold = true,
+      bold = false,
       italic = false,
+    },
+  },
+  integration = {
+    lualine = {
+      enabled = true,
+      -- Letter of lualine section to use (if `nil`, gets detected automatically)
+      mode_section = nil,
+      -- Whether to use lualine's mode highlight's foreground or background
+      highlight = "bg",
     },
   },
 }
